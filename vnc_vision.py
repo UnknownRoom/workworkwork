20672ffd99fc91ce247ffb22e0eea97d55992544
White feathers <xurt697525@163.com>
2026-08-28 15:24:14 +0800

    enable_mkldnn=False,

--- vnc_vision.py
+++ vnc_vision.py
@@ -195,6 +195,7 @@ class VisionEngine:
                 use_doc_orientation_classify=False,
                 use_doc_unwarping=False,
                 use_textline_orientation=False,
+                enable_mkldnn=False,
                 **kwargs,
             )
 
